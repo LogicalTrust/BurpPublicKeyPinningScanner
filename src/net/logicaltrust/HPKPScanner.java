4b79ed327034f88e75d9cbc582bf5903b377d3d8
Michal <michal.dardas@gmail.com>
2017-09-01 09:24:59 +0200

    remove subdomains

--- src/net/logicaltrust/HPKPScanner.java
+++ src/net/logicaltrust/HPKPScanner.java
@@ -18,11 +18,8 @@ public class HPKPScanner implements IScannerCheck {
 
 	private static String HPKP = "Public-Key-Pins: ";
 	private static String HPKP_REPORT_ONLY = "Public-Key-Pins-Report-Only: ";
-	private static String INCLUDE_SUBDOMAINS = "includeSubDomains";
 	
 	private final IExtensionHelpers helpers;
-	private final List<String> subdomains = new ArrayList<>();
-	private final List<String> subdomainsReportOnly = new ArrayList<>();
 	private final List<String> domains = new ArrayList<>();
 
 	public HPKPScanner(IExtensionHelpers helpers) {
@@ -47,39 +44,30 @@ public class HPKPScanner implements IScannerCheck {
 
 		domains.add(host);
 		
-		if (hasSubdomain(subdomainsReportOnly, host)) {
-			return createHpkpReportIssue(url, baseRequestResponse);
-		}
-		
-		if (hasSubdomain(subdomains, host)) {
-			return null;
-		}
-		
 		for (String header : response.getHeaders()) {
 			if (startsWithCaseInsensitive(header, HPKP)) {
-				verifyIncludeSubDomains(HPKP, header, subdomains, host);
 				return null;
 			} else if (startsWithCaseInsensitive(header, HPKP_REPORT_ONLY)) {
-				verifyIncludeSubDomains(HPKP_REPORT_ONLY, header, subdomainsReportOnly, host);
-				return createHpkpReportIssue(url, baseRequestResponse);
+				return createHpkpReportIssue(url, baseRequestResponse, baseRequestResponse);
 			}
 		}
+		
 		return createHpkpIssue(url, baseRequestResponse);
 	}
 	
-	private List<IScanIssue> createHpkpReportIssue(URL url, IHttpRequestResponse baseRequestResponse) {
-		return createIssue("Public key pinning is only reported", url, baseRequestResponse);
+	private List<IScanIssue> createHpkpReportIssue(URL url, IHttpRequestResponse baseRequestResponse, IHttpRequestResponse requestResponse) {
+		return createIssue("Public key pinning report only", url, baseRequestResponse, new IHttpRequestResponse[] { requestResponse });
 	}
 	
 	private List<IScanIssue> createHpkpIssue(URL url, IHttpRequestResponse baseRequestResponse) {
-		return createIssue("Public key pinning not enforced", url, baseRequestResponse);
+		return createIssue("Public key pinning not enforced", url, baseRequestResponse, null);
 	}
 	
-	private List<IScanIssue> createIssue(String title, URL url, IHttpRequestResponse baseRequestResponse) {
+	private List<IScanIssue> createIssue(String title, URL url, IHttpRequestResponse baseRequestResponse, IHttpRequestResponse[] httpMessages) {
 		try {
 			IScanIssue issue = new HPKPScanIssue(title,
 					new URL(url.getProtocol(), url.getHost(), url.getPort(), ""), 
-					null, 
+					httpMessages, 
 					baseRequestResponse.getHttpService());
 			return Arrays.asList(issue);
 		} catch (MalformedURLException e) {
@@ -88,26 +76,8 @@ public class HPKPScanner implements IScannerCheck {
 		}
 	}
 	
-	private boolean hasSubdomain(List<String> subdomains, String host) {
-		for (String subdomain : subdomains) {
-			if (host.endsWith(subdomain)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
 	private boolean startsWithCaseInsensitive(String s1, String s2) {
 		return s1.regionMatches(true, 0, s2, 0, s2.length());
-	}
-	
-	private void verifyIncludeSubDomains(String headerPrefix, String header, List<String> subdomains, String host) {
-		String[] values = header.substring(headerPrefix.length()).split("; ");
-		for (String v : values) {
-			if (INCLUDE_SUBDOMAINS.equals(v)) {
-				subdomains.add("." + host);
-			}
-		}
 	}
 
 	public List<IScanIssue> doActiveScan(IHttpRequestResponse baseRequestResponse,
